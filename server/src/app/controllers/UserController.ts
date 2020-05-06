46e5b82a7bad4b7ef7c90bdeaba69aa67bd417a2
vassao <vlviniciusguaruja7@gmail.com>
2020-05-06 18:33:31 -0300

    api: Updated express router adapter

--- server/src/app/controllers/UserController.ts
+++ server/src/app/controllers/UserController.ts
@@ -1,13 +1,5 @@
-import Context from '../../Context';
-
 export default class UserController {
   static async index(request: HttpRequest, ctx: Context): Promise<HttpResponse> {
-    return {
-      statusCode: 200,
-      body: {
-        message: 'Ok!',
-        ctx,
-      },
-    };
+    return ctx.Ok({});
   }
 }
--- server/src/helpers/global.d.ts
+++ server/src/helpers/global.d.ts
@@ -1,10 +1,25 @@
 import Database from '../database/Database';
+import User from '../app/models/User';
 
 export {};
 
 declare global {
   interface Context {
     db: Database;
+
+    Users: User;
+
+    Ok(body: any): HttpResponse;
+
+    Created(body: any): HttpResponse;
+
+    BadRequest(error?: string): HttpResponse;
+
+    Unauthorized(error?: string): HttpResponse;
+
+    NotFound(error?: string): HttpResponse;
+
+    InternalError(error?: string): HttpResponse;
   }
 
   interface HttpRequest {
