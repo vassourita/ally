3727adfca39ae2417326ca49d10469bb867f0e6b
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-05-24 06:40:00 -0300

    web: Added notifications page

--- web/src/pages/Notifications/index.js
+++ web/src/pages/Notifications/index.js
@@ -1,13 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FiAlertCircle } from 'react-icons/fi';
+import { toast } from 'react-toastify';
 
 import CardBox from '../../components/CardBox';
 import CardHeader from '../../components/CardHeader';
 
+import api from '../../services/api';
+
+import { Container, ListContainer, List, ListItem, Name, Side, Date, LinkButton } from './styles';
+
 function Notifications() {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await api.get('/notifications');
+
+        if (data.notifications) return setNotifications(data.notifications);
+        toast.error('Ocorreu um erro inesperado em nosso servidor');
+      } catch {
+        toast.error('Ocorreu um erro inesperado em nosso servidor');
+      }
+    })();
+  }, []);
+
+  const handleSetRead = async id => {
+    try {
+      const { data } = await api.put(`/notifications/${id}`);
+
+      if (data.updated) {
+        const index = notifications.findIndex(j => j.id === id);
+        const updatedNotifications = [...notifications];
+        updatedNotifications[index] = {
+          ...updatedNotifications[index],
+          is_read: true,
+        };
+        return setNotifications(updatedNotifications);
+      }
+      toast.error('Ocorreu um erro inesperado em nosso servidor');
+    } catch {
+      toast.error('Ocorreu um erro inesperado em nosso servidor');
+    }
+  };
+
   return (
-    <CardBox>
-      <CardHeader title="Notificações" />
-    </CardBox>
+    <Container>
+      <CardBox>
+        <CardHeader title="Notificações" sub="Visualize suas notificações de novas mensagens e propostas" />
+      </CardBox>
+      <ListContainer>
+        <List>
+          {!notifications.length && <h6>Não há notificações...</h6>}
+          {notifications.map(notification => (
+            <ListItem key={notification.id}>
+              <Name>
+                <h4>
+                  {notification.type.name} {!notification.is_read && <FiAlertCircle size="14" color="#df8020" />}
+                </h4>
+                <p>{notification.description}</p>
+              </Name>
+              <Side>
+                <Link to={notification.link}>Ver</Link>
+                {!notification.is_read && (
+                  <>
+                    - <LinkButton onClick={() => handleSetRead(notification.id)}>Marcar como lido</LinkButton>
+                  </>
+                )}
+                <Date>{notification.date}</Date>
+              </Side>
+            </ListItem>
+          ))}
+        </List>
+      </ListContainer>
+    </Container>
   );
 }
 
---
+++ web/src/pages/Notifications/styles.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 40px;
+`;
+
+export const ListContainer = styled.div`
+  background-color: #fff;
+  margin-top: 40px;
+  padding: 20px;
+`;
+
+export const List = styled.ul`
+  h6 {
+    font-size: 15px;
+    font-weight: 400;
+    color: #333;
+  }
+`;
+
+export const ListItem = styled.li`
+  padding: 17px 0;
+  border-top: 1px solid #ccc;
+  :first-child {
+    border-top: none;
+    padding-top: 0;
+  }
+  :last-child {
+    padding-bottom: 0;
+  }
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Name = styled.div`
+  h4 {
+    font-family: 'Quicksand';
+    margin-bottom: 10px;
+  }
+  p {
+    font-size: 12px;
+  }
+`;
+export const Side = styled.div`
+  font-size: 12px;
+  text-align: right;
+  a:hover {
+    text-decoration: underline;
+    color: var(--ally-blue);
+  }
+`;
+export const Date = styled.div`
+  margin-top: 10px;
+`;
+
+export const LinkButton = styled.button`
+  background: none;
+  border: none;
+  font-size: 12px;
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+    color: var(--ally-blue);
+  }
+`;
