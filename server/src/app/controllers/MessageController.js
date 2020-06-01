58039e0a8cb33a9f78ea428afdb2a34c5396a47b
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-06-01 19:40:47 -0300

    server: Added realtime message sending

--- server/src/app/controllers/MessageController.js
+++ server/src/app/controllers/MessageController.js
@@ -1,6 +1,8 @@
 import ChatRepository from '../repositories/ChatRepository';
 import MessageRepository from '../repositories/MessageRepository';
 
+import WebSocket from '../../WebSocket';
+
 export default class MessageController {
   static async index(req, res) {
     const { userId } = req;
@@ -28,6 +30,20 @@ export default class MessageController {
       chat_id: chatId,
       content,
     });
+
+    const chat = await ChatRepository.findOne({
+      where: { id: chatId },
+    });
+
+    const ws = WebSocket.getInstance();
+
+    const targetId = userId === chat.employer_id ? chat.user_id : chat.employer_id;
+
+    const target = ws.connectedUsers[targetId.toString()];
+
+    if (target) {
+      target.connection.emit('new_message', { message });
+    }
 
     return res.status(201).json({ message });
   }
